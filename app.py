964596f817df7abbdb583bc6105124ffeeb41fd7
Angel <75725944+Angel-Girl12@users.noreply.github.com>
2021-08-20 12:57:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
- # Sample.py
- 
- from flask import Flask, render_template, url_for, request, redirect
- 
- app = Flask(__name__)
- 
- @app.route(\'/\')
- def hello_world():
-     return \'hello,world\'
-
- @app.route(\'/user/<username>\', methods=[\'POST\', \'GET\'])
- def user(username):
-     return \'Hello,%s\' % username
- 
- @app.route(\'/user/login\')
- def login():
-     return render_template(\'login.html\')
- 
- @app.route(\'/user/redirect\', methods=[\'POST\'])
- def redirect_to_new_url():
-     username = request.form[\'username\']
-     return redirect(url_for(\'user\',username=username))
- 
- if __name__ == \'__main__\':
-     app.run(debug=True)
+def hello():
+    print("Handling request to home page.")
+    return "Hello, Azure!" 
