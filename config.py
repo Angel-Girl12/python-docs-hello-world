b6cbb8d420e72ebd8d82136310ddd6501ea1e2f8
Angel <75725944+Angel-Girl12@users.noreply.github.com>
2021-08-21 04:05:28 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+# APP调试模式开关
+DEBUG = True
+ 
+'''
+数据库配置
+'''
+# 数据库类型
+SQL = 'mysql'
+# 数据库驱动
+DRIVER = 'pymysql'
+# 数据库主机地址
+HOST = '127.0.0.1'
+# 数据库端口
+PORT = 3306
+# 数据库用户名
+USERNAME = 'root'
+# 数据库密码
+PASSWORD = 'root'
+# 数据库名
+BASENAME = 'test'
+# 编码类型
+CHARSET = 'utf8'
+ 
+# 数据库配置最终变量内容
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset={}" .format(SQL, DRIVER, USERNAME, PASSWORD, HOST, PORT, BASENAME, CHARSET)
+ 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
